3e59d2bdcd7a063aceae3c5e2d88a232371fda80
Robert deCourcy <9704405+NotBruceWayne@users.noreply.github.com>
2021-08-12 19:13:06 -0700

    Update Valve_Control.ino
    Bug fixes for micros() overflow.
    Using 2 pins for module out to reduce stress on microcontroller.
    Better variable names

--- exhaust-valves/Valve_Control/Valve_Control.ino
+++ exhaust-valves/Valve_Control/Valve_Control.ino
@@ -1,26 +1,31 @@
+#include <limits.h>
+
+// Define pins
 // Signal from car
 int car = 2;
 // Signal to module
-int module = 6;
+int module1 = 6;
+int module2 = 7;
 // Signal from remote
 int remote = A0;
 
 // Define constants for signals in and out
-const double period = 31.2;
+const double period = 32;
 const double duty_open = .2;
 const double duty_close = .8;
 
 // Define vars for car duty cycle calculation
 int duty;
-volatile double duty_cycle = .2;
-volatile int prev_time = 0;
+volatile double duty_from_car = .2;
+volatile unsigned long prev_time = 0;
 
 // Define vars for module signal generation
-double sending_duty_cycle;
+double dutyvalves;
 double high_delay = 0;
 double low_delay = 0;
 
 // Define vars for control decision making
+
 int remote_val = 0;
 bool car_control = false;
  
@@ -29,27 +34,14 @@ void setup() {
   
   // Set up pins
   pinMode(car, INPUT);
-  pinMode(module, OUTPUT);
+  pinMode(module1, OUTPUT);
+  pinMode(module2, OUTPUT);
   pinMode(remote, INPUT);
-  
+
   // Begin startup signal
-  digitalWrite(module, LOW);
-  delay(5000);
-  digitalWrite(module, HIGH);          
-  delay(66);
-  digitalWrite(module, LOW);
-  delay(25);
-  for(int x = 0; x<1100/32; x++){
-    duty_cycle = .2;
-    cycle();
-  }
-  for(int x = 0; x<3300/32; x++){
-    duty_cycle = .15;
-    cycle();
-  }
-  // End startup signal
+  startup_singal();
   
-  // When pin car pin goes high, call the rising function
+  // When car pin goes high, call the rising function
   attachInterrupt(digitalPinToInterrupt(car), rising, RISING);
 }
  
@@ -57,13 +49,13 @@ void loop() {
   remote_val = analogRead(remote);
   if(remote_val > 500){
     car_control = false;
-    sending_duty_cycle = duty_close;
-    cycle();
+    dutyvalves = duty_close;
+    delay_cycle();
   }
   else{
     car_control = false;
-    sending_duty_cycle = duty_open;
-    cycle();
+    dutyvalves = duty_open;
+    delay_cycle();
   }
 
 }
@@ -72,18 +64,21 @@ double calc_duty_cycle(double period, double val){
   return double(period*(val/1023))/period;
 }
 
-void cycle(){
-    high_delay = period*sending_duty_cycle;
-    low_delay = period*(1-sending_duty_cycle);
-    digitalWrite(module, HIGH);          
+void delay_cycle(){
+    high_delay = period*dutyvalves;
+    low_delay = period*(1-dutyvalves);
+    digitalWrite(module1, HIGH);
+    digitalWrite(module2, HIGH);         
     floatDelay(high_delay);
-    digitalWrite(module, LOW);
+    digitalWrite(module1, LOW);
+    digitalWrite(module2, LOW);
     floatDelay(low_delay);
 }
  
 void rising() {
   if (car_control){
-    digitalWrite(module, HIGH);
+    digitalWrite(module1, HIGH);
+    digitalWrite(module2, HIGH);
   }
   attachInterrupt(digitalPinToInterrupt(car), falling, FALLING);
   prev_time = micros();
@@ -91,12 +86,40 @@ void rising() {
  
 void falling() {
   if (car_control){
-    digitalWrite(module, LOW);
+    digitalWrite(module1, LOW);
+    digitalWrite(module2, LOW);
   }
   attachInterrupt(digitalPinToInterrupt(car), rising, RISING);
   duty = micros()-prev_time;
-  duty_cycle = double(duty)/(period*1000);
-//  Serial.println(duty_cycle);
+//  Sanity check for micros() overflow
+  if(duty > 0){
+    duty_from_car = double(duty)/(period*1000);
+  }
+  else{
+    duty_from_car = double(duty+ULONG_MAX)/(period*1000);
+  }
+  
+//  Serial.println(duty_from_car);
+}
+
+void startup_singal(){
+  digitalWrite(module1, LOW);
+  digitalWrite(module2, LOW);
+  delay(5000);
+  digitalWrite(module1, HIGH);
+  digitalWrite(module2, HIGH);
+  delay(66);
+  digitalWrite(module1, LOW);
+  digitalWrite(module2, LOW);
+  delay(25);
+  for(int x = 0; x<1100/32; x++){
+    dutyvalves = .2;
+    delay_cycle();
+  }
+  for(int x = 0; x<3300/32; x++){
+    dutyvalves = .15;
+    delay_cycle();
+  }
 }
 
 // Combination of delay ms and us for smoother movement
